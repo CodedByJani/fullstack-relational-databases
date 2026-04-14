2fa954b6050575517f67740c30c49e68f84ae57b
Jani Piirainen <piirainen.jani98@gmail.com>
2026-04-14 10:39:42 +0300

    Exercise 4 done

--- index.js
+++ index.js
@@ -41,15 +41,36 @@ Blog.init(
 const app = express();
 app.use(express.json());
 
+// GET all blogs
 app.get("/api/blogs", async (req, res) => {
-  const blogs = await Blog.findAll();
-  res.json(blogs);
+  try {
+    const blogs = await Blog.findAll();
+    res.json(blogs);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
+// POST a new blog
 app.post("/api/blogs", async (req, res) => {
   try {
     const blog = await Blog.create(req.body);
     res.json(blog);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// DELETE a blog by id
+app.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id);
+    if (blog) {
+      await blog.destroy();
+      res.status(204).end();
+    } else {
+      res.status(404).json({ error: "Blog not found" });
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -61,6 +82,7 @@ sequelize
   .authenticate()
   .then(() => {
     console.log("Database connection successful");
+    Blog.sync();
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
