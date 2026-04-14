80a700b82db4eabc3827961dd877b2d1167b212c
Jani Piirainen <piirainen.jani98@gmail.com>
2026-04-14 11:19:15 +0300

    Exercise 5 done

--- index.js
+++ index.js
@@ -1,92 +1,20 @@
-require("dotenv").config();
 const express = require("express");
-const { Sequelize, Model, DataTypes } = require("sequelize");
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-});
-
-class Blog extends Model {}
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    author: {
-      type: DataTypes.STRING,
-    },
-    url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "blog",
-  },
-);
-
 const app = express();
+
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
+
+const blogsRouter = require("./controllers/blogs");
+
 app.use(express.json());
 
-// GET all blogs
-app.get("/api/blogs", async (req, res) => {
-  try {
-    const blogs = await Blog.findAll();
-    res.json(blogs);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+app.use("/api/blogs", blogsRouter);
 
-// POST a new blog
-app.post("/api/blogs", async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body);
-    res.json(blog);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-// DELETE a blog by id
-app.delete("/api/blogs/:id", async (req, res) => {
-  try {
-    const blog = await Blog.findByPk(req.params.id);
-    if (blog) {
-      await blog.destroy();
-      res.status(204).end();
-    } else {
-      res.status(404).json({ error: "Blog not found" });
-    }
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-const PORT = process.env.PORT || 3001;
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Database connection successful");
-    Blog.sync();
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("Database connection failed:", err);
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
+};
+
+start();
