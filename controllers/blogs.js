c1c8627f96126a01438772ccb3df33031e509149
Jani Piirainen <piirainen.jani98@gmail.com>
2026-04-16 09:48:10 +0300

    Exercise 13 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,16 +1,26 @@
 const router = require("express").Router();
+const { Op } = require("sequelize");
 
 const { Blog, User } = require("../models");
 const { tokenExtractor } = require("../util/middleware");
 
 router.get("/", async (req, res, next) => {
   try {
+    const where = {};
+
+    if (req.query.search) {
+      where.title = {
+        [Op.iLike]: `%${req.query.search}%`,
+      };
+    }
+
     const blogs = await Blog.findAll({
       attributes: { exclude: ["userId"] },
       include: {
         model: User,
         attributes: ["name"],
       },
+      where,
     });
     res.json(blogs);
   } catch (error) {
