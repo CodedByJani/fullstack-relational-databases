a11b4bd591f896f953fab9d1108c3d6a3ce1a8ba
Jani Piirainen <piirainen.jani98@gmail.com>
2026-04-17 13:20:44 +0300

    Exercise 19 trying to fix

--- util/db.js
+++ util/db.js
@@ -18,6 +18,10 @@ const sequelize = new Sequelize(DATABASE_URL, {
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+
+    // 🔥 THIS FIXES "relation does not exist" IN GITHUB ACTIONS
+    await sequelize.sync();
+
     console.log("connected to the database");
   } catch (err) {
     console.error("DB CONNECTION ERROR:");
